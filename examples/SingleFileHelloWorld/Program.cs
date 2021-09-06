0d3c651e644f1a7a8a6e5bc3b8dd005524eb7774
Marcin Nowak <malcinn@gmail.com>
2021-09-06 22:12:56 +0200

    Add example of single file hello world

---
+++ examples/SingleFileHelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using ConsoleApp.Framework;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SingleFileHelloWorld
+{
+    public class Program : IStartup
+    {
+        static Task Main(string[] args)
+            => ConsoleApplication.CreateDefaultBuilder(args).UseStartup<Program>().Build().RunAsync();
+
+        public void ConfigureServices(IServiceCollection services) { }
+
+        public Task StartAsync(IServiceProvider serviceProvider)
+        {
+            Console.WriteLine("Hello world!");
+
+            return Task.CompletedTask;
+        }
+    }
+}
